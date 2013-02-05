3899f466bd3205f7b5ee8b428f9211265cc9026e
fangjue23303 <fangjue23303@c1a4206d-cb94-e041-4141-907c9eedc902>
2013-02-05 09:17:24 +0000

    Update docs

---
+++ docs/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./update_static.sh
+
+current_dir=$PWD
+
+cd ../chromium/crxdoczh/src/chrome/common/extensions/docs/templates/public/extensions/
+for file in *; do
+  ../../../server2/preview.py -r extensions/$file > $current_dir/extensions/$file
+done
+cd -
