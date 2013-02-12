357f0dccb4cabc83433d3ad0d68bb4bd6e0d65ae
fangjue23303 <fangjue23303@c1a4206d-cb94-e041-4141-907c9eedc902>
2013-02-12 13:23:08 +0000

    Rebuild docs.\nAlso update update.sh to generate apps docs.

--- docs/update.sh
+++ docs/update.sh
@@ -4,8 +4,19 @@
 
 current_dir=$PWD
 
-cd ../chromium/crxdoczh/src/chrome/common/extensions/docs/templates/public/extensions/
-for file in *; do
-  ../../../server2/preview.py -r trunk/extensions/$file > $current_dir/extensions/$file
-done
-cd -
+# Usage: render_docs {extensions/apps}
+render_docs () (
+  cd ../chromium/crxdoczh/src/chrome/common/extensions/docs/templates/public/$1/
+  for file in *; do
+    echo Rendering $1/$file...
+    ../../../server2/preview.py -r trunk/$1/$file > $current_dir/$1/$file
+  done
+  cd -
+)
+
+echo Rendering extensions docs...
+render_docs extensions
+echo
+echo Rendering apps docs...
+render_docs apps
+
