ae7616fc7640fd4e5732d63e916dc95ae08d719e
fangjue23303 <fangjue23303@c1a4206d-cb94-e041-4141-907c9eedc902>
2013-02-17 02:19:49 +0000

    Branching for 1410
    Only one change to revert (browsingData.json)

--- docs/update_static.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rsync -a --exclude='.svn' ../chromium/crxdoczh/src/chrome/common/extensions/docs/static ./apps/
-rsync -a --exclude='.svn' ../chromium/crxdoczh/src/chrome/common/extensions/docs/static ./extensions/
