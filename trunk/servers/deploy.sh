1c5db9df82be7ff62f8bc4383518f871c357946b
fangjue23303 <fangjue23303@c1a4206d-cb94-e041-4141-907c9eedc902>
2013-02-23 02:56:51 +0000

    slave-docs server: Returns HTTP 503 if rendering fails. It returned 200 and empty response body before and master server cached it without trying again.

--- trunk/servers/deploy.sh
+++ trunk/servers/deploy.sh
@@ -27,44 +27,11 @@ deploy_slave () (
   sed -f $KEYS_SED $COMMON/url_constants.py > $DST/url_constants.py
   sed -f $KEYS_SED $SRC/app.yaml > $DST/app.yaml
   $DST/build_server.py
-  #$APPENGINE_APPCFG update $DST
-  $APPENGINE_APPCFG backends update $DST
+  $APPENGINE_APPCFG update $DST
+  if [ "$1" == samples ]; then
+    $APPENGINE_APPCFG backends update $DST
+  fi
   rm -rf $DST
-)
-
-deploy_slave_samples () (
-  cp slave-samples/app.yaml app.yaml.0
-  SED=s/\<REPLACE_WITH_YOUR_SAMPLES_APP_ID\>/`cat private/SLAVE_SAMPLES_APP_ID`/
-  sed $SED app.yaml.0 > slave-samples/app.yaml
-
-  cp slave-samples/url_constants.py url_constants.py.0
-  SED1=s/\<REPLACE_WITH_YOUR_SAMPLES_APP_ID\>/`cat private/SLAVE_SAMPLES_APP_ID`/
-  SED2=s/\<REPLACE_WITH_YOUR_SAMPLES_API_KEY\>/`cat private/SLAVE_SAMPLES_API_KEY`/
-  sed $SED1 url_constants.py.0 | sed $SED2 > slave-samples/url_constants.py
-
-  $APPENGINE_APPCFG update slave-samples
-  $APPENGINE_APPCFG backends update slave-samples
-  #cat slave-samples/app.yaml slave-samples/url_constants.py | less
-
-  mv app.yaml.0 slave-samples/app.yaml
-  mv url_constants.py.0 slave-samples/url_constants.py
-)
-
-deploy_slave_docs () (
-  cp slave-docs/app.yaml app.yaml.0
-  cp slave-docs/url_constants.py url_constants.py.0
-  SED1=s/\<REPLACE_WITH_YOUR_DOCS_APP_ID\>/`cat private/SLAVE_DOCS_APP_ID`/
-  SED2=s/\<REPLACE_WITH_YOUR_DOCS_API_KEY\>/`cat private/SLAVE_DOCS_API_KEY`/
-  SED3=s/\<REPLACE_WITH_YOUR_SAMPLES_APP_ID\>/`cat private/SLAVE_SAMPLES_APP_ID`/
-  SED4=s/\<REPLACE_WITH_YOUR_SAMPLES_API_KEY\>/`cat private/SLAVE_SAMPLES_API_KEY`/
-  sed $SED1 url_constants.py.0 | sed $SED2 | sed $SED3 | sed $SED4 > slave-docs/url_constants.py
-  sed $SED1 app.yaml.0 | sed $SED2 | sed $SED3 | sed $SED4 > slave-docs/app.yaml
-
-  $APPENGINE_APPCFG update slave-docs
-  #cat slave-docs/url_constants.py slave-docs/app.yaml | less
-
-  mv app.yaml.0 slave-docs/app.yaml
-  mv url_constants.py.0 slave-docs/url_constants.py
 )
 
 prepare_keys
