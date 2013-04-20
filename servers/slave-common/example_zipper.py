fadbab70f86e0b01acfbdd41217a17851c906cbb
fangjue23303 <fangjue23303@c1a4206d-cb94-e041-4141-907c9eedc902>
2013-04-20 06:45:20 +0000

    server2 changes merged to slave-common and ran successfully on slave-samples!

--- servers/slave-common/example_zipper.py
+++ servers/slave-common/example_zipper.py
@@ -9,26 +9,38 @@ from zipfile import ZipFile
 
 import compiled_file_system as compiled_fs
 
+# Increment this if the data model changes for ExampleZipper.
+_VERSION = 1
+
 class ExampleZipper(object):
   """This class creates a zip file given a samples directory.
   """
-  def __init__(self, file_system, cache_factory, base_path):
-    self._base_path = base_path
-    self._zip_cache = cache_factory.Create(self._MakeZipFile,
-                                           compiled_fs.ZIP)
-    self._file_system = file_system
+  def __init__(self, compiled_fs_factory, base_path):
+    self._base_path = base_path.rstrip('/')
+    # Use an IdentityFileSystem here so that it shares a cache with the samples
+    # data source. Otherwise we'd need to fetch the zip files from the cron job.
+    self._file_cache = compiled_fs_factory.GetOrCreateIdentity()
+    self._zip_cache = compiled_fs_factory.Create(self._MakeZipFile,
+                                                 ExampleZipper,
+                                                 version=_VERSION)
 
   def _MakeZipFile(self, base_dir, files):
-    zip_path = os.path.commonprefix(files).rsplit('/', 1)[-2]
-    prefix = zip_path.rsplit('/', 1)[-2]
-    if zip_path + '/manifest.json' not in files:
+    if 'manifest.json' not in files:
       return None
     zip_bytes = BytesIO()
     zip_file = ZipFile(zip_bytes, mode='w')
     try:
-      for name, file_contents in (
-          self._file_system.Read(files, binary=True).Get().iteritems()):
-        zip_file.writestr(name[len(prefix):].strip('/'), file_contents)
+      for file_name in files:
+        file_path = '%s%s' % (base_dir, file_name)
+        file_contents = self._file_cache.GetFromFile(file_path, binary=True)
+        if isinstance(file_contents, unicode):
+          # Data is sometimes already cached as unicode.
+          file_contents = file_contents.encode('utf8')
+        # We want e.g. basic.zip to expand to basic/manifest.json etc, not
+        # chrome/common/extensions/.../basic/manifest.json, so only use the
+        # end of the path component when writing into the zip file.
+        redundant_prefix = '%s/' % base_dir.rstrip('/').rsplit('/', 1)[0]
+        zip_file.writestr(file_path[len(redundant_prefix):], file_contents)
     finally:
       zip_file.close()
     return zip_bytes.getvalue()
@@ -39,4 +51,4 @@ class ExampleZipper(object):
     Paths within the zip file are given relative to and including |path|.
     """
     return self._zip_cache.GetFromFileListing(
-        self._base_path + '/' + path)
+        '%s/%s' % (self._base_path, path.strip('/')))
--- servers/slave-common/subversion_file_system.py
+++ servers/slave-common/subversion_file_system.py
@@ -3,14 +3,14 @@
 # found in the LICENSE file.
 
 import os
+from file_system import FileSystem, FileNotFoundError, StatInfo, ToUnicode
+from future import Future
+import logging
 import re
 import xml.dom.minidom as xml
 from xml.parsers.expat import ExpatError
 from google.appengine.ext import db
 import logging
-
-import file_system
-from future import Future
 from url_constants import SVN_URL
 from appengine_url_fetcher import AppEngineUrlFetcher
 
@@ -105,6 +105,14 @@ class StatUpdater:
       if len(changes) > 0:
         StatUpdater._SetLastRevision(new_revision)
 
+# Sometimes we get bad data and end up caching it. Increment this so that
+# CachingFileSystem (if one is attached) knows to re-fetch.
+#
+# WARNING: This is a VERY EXPENSIVE number to bump. ONLY do so if the data
+# returned by these operations changes, DESPITE WHAT THE PRESUBMIT WARNING
+# MIGHT TELL YOU!
+_VERSION = 1
+
 class _AsyncFetchFuture(object):
   def __init__(self, paths, fetcher, binary):
     # A list of tuples of the form (path, Future).
@@ -126,18 +134,18 @@ class _AsyncFetchFuture(object):
     for path, future in self._fetches:
       result = future.Get()
       if result.status_code == 404:
-        raise file_system.FileNotFoundError(path)
+        raise FileNotFoundError('Got 404 when fetching %s for Get' % path)
       elif path.endswith('/'):
         self._value[path] = self._ListDir(result.content)
       elif not self._binary:
-        self._value[path] = file_system._ToUnicode(result.content)
+        self._value[path] = ToUnicode(result.content)
       else:
         self._value[path] = result.content
     if self._error is not None:
       raise self._error
     return self._value
 
-class SubversionFileSystem(file_system.FileSystem):
+class SubversionFileSystem(FileSystem):
   """Class to fetch resources from src.chromium.org.
   """
   def __init__(self, fetcher, stat_fetcher):
@@ -159,34 +167,57 @@ class SubversionFileSystem(file_system.FileSystem):
           if e.lineno != i + 1))
 
   def _CreateStatInfo(self, html):
+    def inner_text(node):
+      '''Like node.innerText in JS DOM, but strips surrounding whitespace.
+      '''
+      text = []
+      if node.nodeValue:
+        text.append(node.nodeValue)
+      if hasattr(node, 'childNodes'):
+        for child_node in node.childNodes:
+          text.append(inner_text(child_node))
+      return ''.join(text).strip()
+
     dom = self._ParseHTML(html)
-    # Brace yourself, this is about to get ugly. The page returned from viewvc
-    # was not the prettiest.
-    tds = dom.getElementsByTagName('td')
-    a_list = []
-    found = False
-    dir_revision = None
-    for td in tds:
-      if found:
-        dir_revision = td.getElementsByTagName('a')[0].firstChild.nodeValue
-        found = False
-      a_list.extend(td.getElementsByTagName('a'))
-      if (td.firstChild is not None and
-          td.firstChild.nodeValue == 'Directory revision:'):
-        found = True
-    child_revisions = {}
-    for i, a in enumerate(a_list):
-      if i + 1 >= len(a_list):
-        break
-      next_a = a_list[i + 1]
-      name = a.getAttribute('name')
-      if name:
-        rev = next_a.getElementsByTagName('strong')[0]
-        if 'file' in next_a.getAttribute('title'):
-          child_revisions[name] = rev.firstChild.nodeValue
-        else:
-          child_revisions[name + '/'] = rev.firstChild.nodeValue
-    return file_system.StatInfo(dir_revision, child_revisions)
+
+    # Try all of the tables until we find the one that contains the data.
+    for table in dom.getElementsByTagName('table'):
+      # Within the table there is a list of files. However, there may be some
+      # things beforehand; a header, "parent directory" list, etc. We will deal
+      # with that below by being generous and just ignoring such rows.
+      rows = table.getElementsByTagName('tr')
+      child_versions = {}
+
+      for row in rows:
+        # Within each row there are probably 5 cells; name, version, age,
+        # author, and last log entry. Maybe the columns will change; we're at
+        # the mercy viewvc, but this constant can be easily updated.
+        elements = row.getElementsByTagName('td')
+        if len(elements) != 5:
+          continue
+        name_element, version_element, _, __, ___ = elements
+
+        name = inner_text(name_element)  # note: will end in / for directories
+        try:
+          version = int(inner_text(version_element))
+        except ValueError:
+          continue
+        child_versions[name] = version
+
+      if not child_versions:
+        continue
+
+      # Parent version is max version of all children, since it's SVN.
+      parent_version = max(child_versions.values())
+
+      # All versions in StatInfo need to be strings.
+      return StatInfo(str(parent_version),
+                      dict((path, str(version))
+                           for path, version in child_versions.iteritems()))
+
+    # Bleh, but, this data is so unreliable. There are actually some empty file
+    # listings caused by git/svn/something not cleaning up empty dirs.
+    return StatInfo('0', {})
 
   def _InitStatInfo(self, directory, svn_listing):
     files = _ListGoogleCodeSvnDir(svn_listing)
@@ -206,7 +237,7 @@ class SubversionFileSystem(file_system.FileSystem):
       value = cur.split(':')
       child_revisions[value[0]] = value[1]
     return file_system.StatInfo(model.revision, child_revisions)
-    
+
   def Stat(self, path):
     directory = path.rsplit('/', 1)[0]
     if os.environ.get('CRXDOCZH_SLAVE_TYPE') == 'docs':
@@ -217,19 +248,25 @@ class SubversionFileSystem(file_system.FileSystem):
       if result is None:
         result = self._stat_fetcher.Fetch(directory + '/')
         if result.status_code == 404:
-          raise file_system.FileNotFoundError(path)
+          raise ileNotFoundError(
+              'Got 404 when fetching %s from %s for Stat' % (path, directory))
         stat_info = self._InitStatInfo(full_path, result.content)
       else:
         stat_info = self._StatFromModel(result)
     else:
       result = self._stat_fetcher.Fetch(directory + '/')
       if result.status_code == 404:
-        raise file_system.FileNotFoundError(path)
+        raise FileNotFoundError(
+            'Got 404 when fetching %s from %s for Stat' % (path, directory))
       stat_info = self._CreateStatInfo(result.content)
     if not path.endswith('/'):
       filename = path.rsplit('/', 1)[-1]
       if filename not in stat_info.child_versions:
-        raise file_system.FileNotFoundError(path)
+        raise FileNotFoundError('%s was not in child versions' % filename)
       stat_info.version = stat_info.child_versions[filename]
     logging.info('Stat: %s of %s' % (path, stat_info.version))
     return stat_info
+
+  @classmethod
+  def GetVersion(cls):
+    return _VERSION
