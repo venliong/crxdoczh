3693bd12a26f9c7617ff33f29fadc498ecb4aeec
fangjue23303 <fangjue23303@c1a4206d-cb94-e041-4141-907c9eedc902>
2014-05-22 06:56:04 +0000

    DO NOT UPDATE SERVER NOW.

--- chromium/crxdoczh/update.sh
+++ chromium/crxdoczh/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -a --exclude='.svn' --exclude='.git' --exclude='*.cc' --exclude='*.h' --exclude='server2' --exclude='server_static' --exclude='examples' ../official/src ./
+rsync -a --exclude='.svn' --exclude='.git' --exclude='*.cc' --exclude='*.h' --exclude='server2' --exclude='server_static' --exclude='examples' --exclude='devtools-legacy' ../official/src ./
 svn status
 
